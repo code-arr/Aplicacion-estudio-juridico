e638e9c5ef0744c2b353f722bead92b78f34c9c9
benjamin del campo <benjadelcampo15@gmail.com>
2025-07-13 03:00:19 -0300

    base de datos creada

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import typeormConfig from './config/typeorm';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm'; // <-- Importa esto aquí también
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [typeormConfig] }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        // Usa el operador de aserción no nula '!' si estás seguro de que la configuración existe
+        const options = configService.get<TypeOrmModuleOptions>('typeorm')!;
+        return options;
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ back/src/config/typeorm.ts
@@ -0,0 +1,28 @@
+/* eslint-disable prettier/prettier */
+import { config as dotenvConfig } from 'dotenv';
+import { registerAs } from '@nestjs/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+dotenvConfig({ path: '.env' });
+
+const config: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['dist/src/migrations/*{.ts,.js}'],
+  migrationsTableName: 'migrations',
+  logging: ['error'],
+  migrationsRun: true,
+  synchronize: true,
+  // dropSchema: true,
+};
+
+export default registerAs('typeorm', () => config);
+
+export const connectDataSource = () =>
+  new DataSource(config as DataSourceOptions);
