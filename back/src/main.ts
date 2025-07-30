53140fde765c36ba8d87617ae0bb5be35f012ebd
benjamin del campo <benjadelcampo15@gmail.com>
2025-07-30 15:36:39 -0300

    habilitar cors

--- back/src/main.ts
+++ back/src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: "*", // o un dominio específico
+  });
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
