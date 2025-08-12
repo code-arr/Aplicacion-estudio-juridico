f4691fb6c54594e3c2a1f3acbb64db78ddc8518e
benjamin del campo <benjadelcampo15@gmail.com>
2025-08-12 18:04:38 -0300

    cambios en los controladores

---
+++ back/src/repositories/clientItem.repository.ts
@@ -0,0 +1,8 @@
+// import { InjectRepository } from "@nestjs/typeorm";
+// import { ClientItem } from "src/entities/clientItem.entity";
+
+// export class ClientItemRepository {
+//     constructor(@InjectRepository(ClientItem) ){}
+
+
+// }
