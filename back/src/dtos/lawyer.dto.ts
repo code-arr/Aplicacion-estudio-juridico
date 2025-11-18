01863eaa456b24d551cc5f9b557648b52a8e19d5
benjamin del campo <benjadelcampo15@gmail.com>
2025-11-18 03:49:17 -0300

    a

--- back/src/dtos/lawyer.dto.ts
+++ back/src/dtos/lawyer.dto.ts
@@ -16,7 +16,7 @@ export class AbogadoDto {
   lastName: string;
 
   @IsString()
-  @IsNotEmpty()
+  @IsOptional()
   @MaxLength(100)
   address: string;
 
@@ -38,7 +38,7 @@ export class AbogadoDto {
   type: lawyerType.CRIMINAL | lawyerType.CIVIL | lawyerType.FAMILIAR;
 
   @IsEnum(seniorityLevel, { message: 'El nivel de seniority no es válido.' })
-  @IsNotEmpty() // Asumo que el seniority es obligatorio al crear
+  @IsOptional() // Asumo que el seniority es obligatorio al crear
   seniorityLevel: seniorityLevel.JUNIOR | seniorityLevel.MID | seniorityLevel.SENIOR;
 
   @IsInt()
