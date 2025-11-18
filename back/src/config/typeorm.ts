67319fa43fa7ce15696affb7c1b63bcd85d6b02f
benjamin del campo <benjadelcampo15@gmail.com>
2025-11-18 03:53:56 -0300

    a

--- back/src/config/typeorm.ts
+++ back/src/config/typeorm.ts
@@ -18,7 +18,7 @@ const config: TypeOrmModuleOptions = {
   migrationsTableName: 'migrations',
   logging: ['error'],
   migrationsRun: true,
-  synchronize: false,
+  synchronize: true,
   //dropSchema: true,
   ssl: {
     rejectUnauthorized: false, // ✅ Esto habilita SSL aunque no tengas certificado local
