74413769a8ab3441d87583815292b2e09f5b9803
Tomas Guignet <tomasguignet9@gmail.com>
2025-12-24 20:31:24 -0300

    Fix comma error

--- back/src/utils/abogados.ts
+++ back/src/utils/abogados.ts
@@ -1,5 +1,4 @@
-import {  , lawyerType } from "../entities/lawyer.entity";
-
+import { lawyerType } from '../entities/lawyer.entity';
 
 export const abogadosSeedData = [
   {
@@ -8,7 +7,7 @@ export const abogadosSeedData = [
     address: 'Calle Falsa 123',
     phone: '1122334455',
     rut: '12345678-9',
-    type: lawyerType.CIVIL, 
+    type: lawyerType.CIVIL,
     workedHours: 1500,
     userEmail: 'abogado3@example.com',
     // No incluyas 'id', 'usuario', 'casos', 'cronometros', o 'clientes' aquí.
@@ -21,9 +20,9 @@ export const abogadosSeedData = [
     phone: '6677889900',
     rut: '98765432-1',
     type: lawyerType.CRIMINAL,
-   
+
     horasTrabajadas: 800,
-    userEmail : 'benjadelcampo15@gmail.com'
+    userEmail: 'benjadelcampo15@gmail.com',
   },
   {
     firstName: 'Sofía',
@@ -32,8 +31,8 @@ export const abogadosSeedData = [
     phone: '2233445566',
     rut: '11223344-5',
     type: lawyerType.FAMILIAR,
-  
+
     horasTrabajadas: 200,
-    userEmail: 'abogado2@example.com'
+    userEmail: 'abogado2@example.com',
   },
 ];
